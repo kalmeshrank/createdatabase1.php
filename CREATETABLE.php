ec49ab535b1183db2a825b4ac180ad88d9940df6
kamleshrank <118710589+kalmeshrank@users.noreply.github.com>
2023-04-07 22:15:36 -0700

    Create CREATETABLE.php

---
+++ CREATETABLE.php
@@ -0,0 +1,7 @@
+<?php
+   $con=mysqli_connect("localhost","root","","my_db");
+   $sql="CREATE TABLE table1(Username CHAR(30),Password CHAR(30),Role CHAR(30))";
+   if (mysqli_query($con,$sql)) {
+      echo "Table have been created successfully";
+   }
+?>
